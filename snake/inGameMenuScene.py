34bba3a6d5a64087b53bca653006c0d1176a29fc
Justine Yang <justine.yang@epitech.eu>
2022-04-28 10:36:44 +0200

    feat: in-game menu buttons interface

--- snake/inGameMenuScene.py
+++ snake/inGameMenuScene.py
@@ -1,18 +1,32 @@
 from sceneBase import SceneBase
 from utils import *
 from gameMap import GameMap
-
+from button import Button
 
 class InGameMenuScene(SceneBase):
     gameMap = GameMap()
+    resumeButton = Button("Resume", 450, 200)
+    restartButton = Button("Restart", 455, 300)
+    saveButton = Button("Save", 480, 400)
+    exitButton = Button("Exit", 490, 500)
 
     def __init__(self):
         SceneBase.__init__(self)
         pygame.display.set_caption("Snake in-game menu")
 
     def ProcessInput(self, events, pressed_keys):
-        # TODO: Game Controller
-        pass
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                if pygame.mouse.get_pressed()[0]:
+                    if self.resumeButton.rect.collidepoint(x,y):
+                        print("resume")
+                    elif self.restartButton.rect.collidepoint(x,y):
+                        print("restart")
+                    elif self.saveButton.rect.collidepoint(x,y):
+                        print("save")
+                    elif self.exitButton.rect.collidepoint(x,y):
+                        print("exit")
 
     def Update(self):
         # TODO: Game Logique Update
@@ -20,3 +34,7 @@ class InGameMenuScene(SceneBase):
 
     def Render(self):
         App.screen.fill(pygame.Color("Black"))
+        self.resumeButton.show()
+        self.restartButton.show()
+        self.saveButton.show()
+        self.exitButton.show()
